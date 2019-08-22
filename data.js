054de8bf1d4d12f7a894f9d9e8b116f77b1a5158
Vivekanand P V <vivekanandpv@outlook.com>
2019-08-22 23:00:22 +0530

    Working copy

--- data.js
+++ data.js
@@ -1,24 +1,31 @@
-const BUGS = [
+const POLL_ITEMS = [
   {
     id: 1,
-    createdAt: new Date(),
-    severity: 0,
-    comments: '',
-    isResolved: false,
-    description: 'A severe bug is encountered',
-    title: 'Cannot login using test credentials',
-    user: 'Rajeev'
+    title: 'Foosball',
+    group: 'Development',
+    imageUrl:
+      'https://images.homedepot-static.com/productImages/0b939c73-0b96-4dbe-ae23-d156aceb4b73/svn/sunnydaze-decor-foosball-tables-dq-s028-64_1000.jpg',
+    description: 'Foosball for entertainment',
+    voteCount: 0
   },
   {
     id: 2,
-    createdAt: new Date(),
-    severity: 3,
-    comments: '',
-    isResolved: true,
-    description: 'Normal bug is encountered',
-    title: 'Blah blah',
-    user: 'Mahesh'
+    title: 'Gym',
+    group: 'Marketing',
+    imageUrl:
+      'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    description: 'Gym for fitness and strength training',
+    voteCount: 0
+  },
+  {
+    id: 3,
+    title: 'Snack Corner',
+    group: 'Operations',
+    imageUrl:
+      'https://www.walesnationalpoolswansea.co.uk/wp-content/uploads/snack-corner.jpg',
+    description: 'Snack corner for refreshment and healthful eating',
+    voteCount: 0
   }
 ];
 
-module.exports = BUGS;
+module.exports = POLL_ITEMS;
--- routes/rest.js
+++ routes/rest.js
@@ -1,37 +1,78 @@
 var express = require('express');
 var router = express.Router();
-var bugs = require('../data');
+var poll_items = require('../data');
 
 /* GET home page. */
 router.get('/get', function(req, res, next) {
-  res.send(bugs);
+  res.send(poll_items);
 });
 
 router.get('/get/:id', function(req, res, next) {
   let id = +req.params.id;
-  res.send(bugs.filter(bug => bug.id === id));
+  res.send(poll_items.filter(p => p.id === id));
+});
+
+router.get('/votes/:id', (req, res) => {
+  let id = +req.params.id;
+  let poll_item = poll_items.filter(p => p.id === id)[0];
+  if (!poll_item) {
+    res.sendStatus(400);
+  } else {
+    res.send({ voteCount: poll_item.voteCount });
+  }
+});
+
+router.get('/votes', (req, res) => {
+  let total = 0;
+  poll_items.forEach(p => {
+    total += +p.voteCount;
+  });
+
+  res.send({ totalVotes: total });
+});
+
+router.get('/groups', (req, res) => {
+  res.send({ groups: ['Development', 'Marketing', 'Operations'] });
 });
 
 router.post('/create', function(req, res, next) {
-  const bug = req.body;
-  bugs.push(bug);
-  res.send('Add operation successful');
+  const poll_item = req.body;
+  poll_items.push(poll_item);
+  res.send({ message: 'Add operation successful' });
+});
+
+router.put('/vote/:id', (req, res) => {
+  let id = +req.params.id;
+  let item = poll_items.filter(p => p.id === id)[0];
+  if (!item) {
+    res.sendStatus(400);
+  } else {
+    item.voteCount++;
+    res.send({ message: `Voted successfully for ${item.title}` });
+  }
 });
 
 router.put('/update', function(req, res, next) {
-  const bug = req.body;
-  const id = +bug.id;
-  const index = bugs.indexOf({ id: +bug.id });
-  bugs[index] = bug;
-  res.send('Update operation successful');
+  const poll_item = req.body;
+  const index = poll_items.indexOf({ id: +poll_item.id });
+
+  if (index < 0) {
+    res.sendStatus(400);
+  } else {
+    poll_items[index] = poll_item;
+    res.send({ message: 'Update operation successful' });
+  }
 });
 
 router.delete('/delete/:id', function(req, res, next) {
   let id = +req.params.id;
-  const index = bugs.indexOf({ id });
-  bugs = bugs.splice(index, 1);
-
-  res.send('Delete operation successful');
+  const index = poll_items.indexOf({ id });
+  if (index < 0) {
+    res.sendStatus(400);
+  } else {
+    poll_items = poll_items.splice(index, 1);
+    res.send({ message: 'Delete operation successful' });
+  }
 });
 
 module.exports = router;
